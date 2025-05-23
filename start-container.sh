48393bfb37c5f6d5ad26e2e9c5e0b849ce904d28
YLG-dev <youenn.legouedec@gmail.com>
2025-05-23 18:18:40 +0200

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -4,13 +4,16 @@ set -e
 mv .env.example .env
 
 for secret_file in /run/secrets/*; do
-    name=$(basename $secret_file | sed "s/^${APP_NAME}_//")
-    value=$(cat $secret_file)
-    sed -i "s|^${name}=.*|${name}=${value}|" /app/.env
+    name=$(basename "$secret_file" | sed "s/^${APP_NAME}_//")
+    value=$(cat "$secret_file")
+    if grep -q "^${name}=" /app/.env; then
+        sed -i "s|^${name}=.*|${name}=${value}|" /app/.env
+    else
+        echo "${name}=${value}" >>/app/.env
+    fi
 done
 
 php /app/artisan storage:link
 php /app/artisan optimize
-# rm /app/.env
 
 exec "$@"
